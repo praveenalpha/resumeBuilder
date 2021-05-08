ba057c03bc57bc144bc11879b896e04b57927487
Praveen Mani <48654802+praveenalpha@users.noreply.github.com>
2021-05-08 18:39:12 +0530

    Delete src/components directory

--- src/components/actions/documentAction.js
+++
@@ -1,6 +0,0 @@
-export const documentAction = (skinCode) => {
-    return(dispatch, getState, {getFirebase, getFirestore})=>{
-        console.log("inside documetnAction ", skinCode)
-        dispatch({type: "CHANGE_SKIN", skinCode: skinCode});
-    }
-}
